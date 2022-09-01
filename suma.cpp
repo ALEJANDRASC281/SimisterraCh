de8b8f27fab9688263ee4d78df665f1c4eb1bc3d
SimisterraAlejandra <alejandra.simisterra@utelvt.edu.ec>
2022-09-01 14:07:50 -0500

    programa suma

--- suma.cpp
+++ suma.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-     cout<<endl<<endl;
-     cout<<"hola mundo";
-     cout<<endl<<endl;
-
+     float x,y,z;
+     cout<<"Ingresd dos numeros x y y : ";
+     cin>>x>>y;
+     z=x+y;
+     cout<<"El resultado es: ";
+     cout<<z;
      return 0;
 }
