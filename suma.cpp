84d73919e4b0567637818f9b516e5851883cbc08
SimisterraAlejandra <alejandra.simisterra@utelvt.edu.ec>
2022-09-10 17:46:18 -0500

    cree un programa en c++

--- suma.cpp
+++ suma.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
      float A,B,C;
+     cout<<"Ingrese 2 numeros A y B: ";
      cin>>A>>B;
      C=A+B;
-     cout<<C;
+     cout<<"El resultado es: ";
+     cout<<C<<endl;
      return 0;
 }
